d3a5917db333afdcb189d3b8962948dad6a08a89
jmetais2017 <julien.metais@eleves.ec-nantes.fr>
2021-02-05 22:53:19 -0500

    relecture

--- TD1/Exercice1/Exo1.cpp
+++ TD1/Exercice1/Exo1.cpp
@@ -1,74 +1,64 @@
 /**
-* Programme qui affiche le mot le plus court, le mot le plus long et la longueur moyenne d'une phrase
+* Programme qui affiche le mot le plus court, le mot le plus long et la longueur moyenne des mots d'une phrase saisie au clavier
 * \file   Exo1.cpp
 * \author Maya Kurdi-Teylouni et Julien Métais
 * \date   5 février 2021
 * Créé le 26 janvier 2021
 */
 
-
 #include <string>
 #include <iostream>
 #include <sstream>
-#include <vector>
-
-#include <tuple>
 
 using namespace std;
 
 int main()
 {
     string phrase;
-    std::cout << "Ecrivez une phrase svp";//L'utilisateur écrit une phrase
+    std::cout << "Ecrivez une phrase svp\n"; // L'utilisateur écrit une phrase.
     getline(cin, phrase);
-    
     
     string motCourt = phrase;
     string motLong = "";
     string motCourant = "";
-    string motCourant1 = "";
-    double nMots = 0.0;
-    double longueurMoy = 0.0;
+    double longueurTotale = 0.0;
+    int nMots = 0;
+    double longueurMoyenne = 0.0;
 
-    //on passe chaque caractère de la phrase
-    for (int i = 0; i < phrase.length(); i++)
+    // On passe chaque caractère de la phrase :
+    for (int i = 0; i < (int)phrase.length(); i++)
     {
         int result = isspace(phrase[i]);
-        if (result==0)//si le caractère actuel est un espace
+        if (result == 0) // Si le caractère courant est un espace :
         {
             motCourant += phrase[i];
         }
-        if (result==8|| i == phrase.length()-1)/*si le caractère actuel n'est 
-            pas un espace ou si on est au dernier caractère de la phrase*/
+        if (result == 8 || i == (int)phrase.length() - 1) // Si le caractère actuel n'est pas un espace ou si on est au dernier caractère de la phrase :
         {
-
-            if (motCourant.length() < motCourt.length())/*Mise à jour de la valeur 
-                du mot le plus court de la phrase*/
+            if (motCourant.length() < motCourt.length()) // Mise à jour de la valeur du mot le plus court de la phrase.
             {
                 motCourt = motCourant;
             }
-            if (motCourant.length() > motLong.length())/*Mise à jour de la valeur 
-                du mot le plus long de la phrase*/
+            if (motCourant.length() > motLong.length()) // Mise à jour de la valeur du mot le plus long de la phrase.
             {
                 motLong = motCourant;
             }
-            motCourant1 += motCourant;
+
+            longueurTotale += motCourant.length();
             nMots += 1;
             motCourant = "";
-                if (nMots != 0) //calcul de la longueur moyenne des mots dans la phrase
-                    {
-                    longueurMoy = motCourant1.length() / nMots;
-                    }
-            
 
+            if (nMots != 0) // Calcul de la longueur moyenne des mots dans la phrase.
+            {
+                longueurMoyenne = longueurTotale / double(nMots);
+            }    
         }
     }
-    //Affichage du mot court, mot long et de la longueur moyenne
-    cout << "Le mot le plus court est : " << motCourt << "\n";
 
-    cout << "Le mot le plus long est :" << motLong << "\n";
-
-    cout << "La longueur moyenne est :" << longueurMoy << "\n";
+    //Affichage du mot le plus court, du mot le plus  long et de la longueur moyenne
+    std::cout << "Le mot le plus court est : " << motCourt << "\n";
+    std::cout << "Le mot le plus long est : " << motLong << "\n";
+    std::cout << "La longueur moyenne est : " << longueurMoyenne << "\n";
    
     return 0;
 
--- TD1/Exercice2/Exo2.cpp
+++ TD1/Exercice2/Exo2.cpp
@@ -6,47 +6,28 @@
 * Créé le 26 janvier 2021
 */
 
-
 #include <iostream>
 #include <iomanip>  
 
 using namespace std;
 
-double limite(double precision) //fonction qui calcule la limite de la suite
+double calculeLimite(int precision) // Fonction qui calcule la limite de la suite avec un nombre de chiffres après la virgules passé en argument.
 {
+	double uPresent = 1.0; // Le terme initial U0 = 1 de la suite.
+	double uProchain = pow((2 + uPresent), 0.5); // Calcul du prochain terme de la suite.
 
-	double uPresent = 1.0;//le terme initial U0 de la suite=1
-	
-	double uProchain = pow((2 + uPresent), 0.5);//Calcul du prochain terme de la suite
-	while ((uProchain - uPresent) > 0.00001)/*Calcul du prochain terme tant que
-		la différence entre le terme prochain 
-		et le terme présent est supérieure à la précision de la limite*/
+	while((uProchain - uPresent) > pow(10, -precision)) // Calcul des termes de la suite tant que la différence entre 2 termes consécutifs est supérieure à la précision voulue.
 	{
 		uPresent = uProchain;
 		uProchain = pow((2 + uPresent), 0.5);
-	
-
-			if ((uProchain - uPresent) <= 0.00001) /*quand la différence entre terme 
-				prochain et terme présent est inférieure ou égale à la précision 
-				de la limite, on retourne le dernier uprochain calculé comme limite*/
-			{
-			
-				uProchain = (double)(uProchain / precision) * precision;
-				return uProchain;
-			}
-			
-
 	}
-	
+	return uProchain;
 }
 
 int main() //Affichage de la limite dans main
 {
+	int precision = 5; // 5 chiffres après la virgule.
 	
-	double precision = 5.0;//5 chiffres après la virgule
-	double uProchain = limite(precision);
-	
-	cout << "La limite de cette suite est de " << setiosflags(ios::fixed) << setprecision(precision) << limite(precision);
-
+	cout << "La limite de cette suite est de " << setiosflags(ios::fixed) << setprecision(precision) << calculeLimite(precision) << endl;
 }
 
